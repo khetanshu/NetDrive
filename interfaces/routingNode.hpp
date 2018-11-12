1d1e9bfafbd182965659ae547a5acb2d150c8ab4
khetanshu <khetanshu.chauhan@gmail.com>
2018-11-11 20:00:04 -0800

    nodes update

--- interfaces/routingNode.hpp
+++ interfaces/routingNode.hpp
@@ -18,16 +18,24 @@ class RoutingNode {
     //Data structures
     int totalStorageNodes; //Keeps track of total storage nodes
     long totalRemainingStorageCapacity; //Keeps track of total remaining storage capacity in the system {as a whole}
-    unordered_map<int,int> nodeRemainingStorageCapacity; //Keeps track of the the remaining storage capacity of each storage node
+    unordered_map<int,int> nodeCapacityIndex; //Keeps track of the the remaining storage capacity of each storage node
     int lastestChunkID; //Keeps track of the latest ID assigned to the file's chunks. This is a unique ID assigned to each chunks in the system
-    unordered_map<string,vector<chunk>> fileToChunksIndex;//Keeps the chunks information of each files within the system
+    unordered_map<string,vector<chunk>> fileIndex;//Keeps the chunks information of each files within the system
     
 private:
+    //
     vector<chunk> generateChunkInfo(string filename, int size);
     
 public:
+    //Returns true if totalRemainingStorageCapacity <= newFileSize otherwise false
     bool canStore(int newFileSize);
+    
+    //If the size of the input file is less than then total available system's storage capacity
+    //then module would return the information of the chunks; Client would need to divide the the file into
+    //chunks as stated in the passed infomration and trasnfer those to the storage nodes
     vector<chunk> storeFile(string filename, int size);
+    
+    //Returns the chunks information of the input file
     vector<chunk> retrieveFilestring (string filename);
 };
 
--- services/RoutingNode.cpp
+++ services/RoutingNode.cpp
@@ -6,28 +6,38 @@
 
 #include "routingNode.hpp"
 
-vector<chunk> RoutingNode::generateChunkInfo(string filename, int size){
-    vector<chunk> chunks;
-    //TO:DO
-    
-    return chunks;
-}
 
 bool RoutingNode::canStore(int newFileSize){
     return (newFileSize > totalRemainingStorageCapacity)? false:true;
 }
 
+
+vector<chunk> RoutingNode::generateChunkInfo(string filename, int size){
+    vector<chunk> chunks;
+    //TO:DO
+    
+    
+    
+    
+    
+    
+    
+    
+    return chunks;
+}
+
 vector<chunk> RoutingNode::storeFile(string filename, int size){
     vector<chunk> chunks;
     if(canStore(size)){
-        fileToChunksIndex[filename] = generateChunkInfo(filename, size);
-        return fileToChunksIndex[filename];
+        fileIndex[filename] = generateChunkInfo(filename, size);
+        return fileIndex[filename];
     }
     return chunks;
 }
 
+
 vector<chunk> RoutingNode::retrieveFilestring (string filename){
-    return fileToChunksIndex[filename];
+    return fileIndex[filename];
 }
 
 
