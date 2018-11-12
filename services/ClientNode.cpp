18a2d3c2c2a90daa06e1c40a53e56a895d127c0c
jayantkashyap <jayant.kashyap1@gmail.com>
2018-11-11 19:50:46 -0800

    added splitFile feature

--- services/ClientNode.cpp
+++ services/ClientNode.cpp
@@ -5,17 +5,57 @@
 //
 
 #include "clientNode.hpp"
-<<<<<<< HEAD
 #include "stdexcept"
 #include "fstream"
 #include "string"
 #include "vector"
 
 
-=======
 
 
+void clientNode::splitFile(string fileName, vector<int>& sizes)
+{
+    int peek = 0, i = 0;
+    string line;
+    string buffer = "";
+    
+//  reading file to buffer
+    try
+    {
+        ifstream i_file(file_name);
+        
+        while (!i_file.eof())
+        {
+            getline(i_file, line);
+            buffer+=line;
+            buffer+="\n";
+        }
+        i_file.close();
+    }
+    catch (exception const& e)
+    {
+        cerr << "Error occured while reading file!\n" << e.what() << endl;
+    }
 
+
+//  splitting file into chunks
+    for(auto &size: sizes) 
+    {
+        try
+        {   
+            ofstream o_file(fileName+"-"+to_string(i++));
+            o_file << buffer.substr(peek, size);
+            o_file.close();
+            peek+=size;
+            cout << endl;
+        }
+        catch (exception const& e)
+        {
+            cerr << "Error ocurred while writing file!\n" << e.what() << endl; 
+        }
+    }
+
+} 
 
 
 bool ClientNode::transferFileToCloud(vector<chunk>& chunks){
@@ -27,4 +67,3 @@ bool ClientNode::transferChunkToStorageNode(chunk chunk){
     //TODO
     return true;
 }
->>>>>>> b9e461ad9aee3e8d32dc261dd07fb0fc43041c18
